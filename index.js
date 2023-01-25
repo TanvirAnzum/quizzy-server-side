dce92734b65453a0d6152e9f8b09601102b87006
TanvirAnzum <tanviranzum14@gmail.com>
2023-01-25 11:18:33 +0600

    'added'

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ function verifyJWT(req, res, next) {
 async function run() {
   try {
     const db = client.db("quizzesDB").collection("quizzes");
+    const testsCollection = client.db("quizzesDB").collection("tests");
 
     /// jwt token request
     app.post("/jwt", (req, res) => {
@@ -79,10 +80,9 @@ async function run() {
       const userEmail = req.query.email;
       const limit = Number(req.query.limit);
       const page = Number(req.query.page);
+      const status = req.query.status;
 
       const { email } = req.decoded || {};
-
-      console.log(userEmail, email);
 
       if (email !== userEmail) {
         res.status(403).send({ message: "unauthorized access" });
@@ -90,6 +90,8 @@ async function run() {
         const query = {
           author: userEmail,
         };
+        if (status === "pending" || status === "published")
+          query.status = status;
 
         const totalItems = await db.countDocuments(query);
 
@@ -108,35 +110,27 @@ async function run() {
 
     // for getting partcipated quizzes
 
-    app.get("/quizzes/:email", verifyJWT, async (req, res) => {
+    app.get("/quizzes/:email", async (req, res) => {
       const userEmail = req.params.email;
       const limit = Number(req.query.limit);
       const page = Number(req.query.page);
 
-      const { email } = req.decoded || {};
+      const query = {
+        participants: { $in: [userEmail] },
+      };
 
-      console.log(userEmail, email);
+      const particaptedQuizCursor = db
+        .find(query)
+        .skip(page ? (limit ? page * limit : 0) : 0)
+        .limit(limit ? limit : 10);
 
-      if (email !== userEmail) {
-        res.status(403).send({ message: "unauthorized access" });
-      } else {
-        const query = {
-          participants: { $in: [userEmail] },
-        };
+      const totalParticipatedQuiz = await db.countDocuments(query);
+      const participatedQuiz = await particaptedQuizCursor.toArray();
 
-        const particaptedQuizCursor = db
-          .find(query)
-          .skip(page ? (limit ? page * limit : 0) : 0)
-          .limit(limit ? limit : 10);
-
-        const totalParticipatedQuiz = await db.countDocuments(query);
-        const participatedQuiz = await particaptedQuizCursor.toArray();
-
-        res.send({
-          quizzes: participatedQuiz,
-          totalCount: totalParticipatedQuiz,
-        });
-      }
+      res.send({
+        quizzes: participatedQuiz,
+        totalCount: totalParticipatedQuiz,
+      });
     });
 
     app.get("/quiz/:id", async (req, res) => {
@@ -198,11 +192,62 @@ async function run() {
       res.send(result);
     });
 
-    // for quiz exams
-    // get request
+    // create new test
+    app.post("/test", async (req, res) => {
+      const data = req.body;
+      const email = req.query.email;
+      const quizId = req.query.quizId;
+      const response = await testsCollection.findOne({ quizId, email });
+      console.log(response);
+      if (response?._id) {
+        res.status(403).send({ message: "This user already given the test" });
+      } else {
+        const result = await testsCollection.insertOne(data);
+        res.send(data);
+      }
+    });
 
-    app.get("/test/:id", async (req, res) => {
-      const id = req.params.id;
+    // get test
+    app.get("/test", async (req, res) => {
+      const testId = req.query.id;
+      const email = req.query.email;
+      console.log(testId);
+
+      const query = {};
+      if (testId) {
+        query._id = ObjectID(testId);
+        const response = await testsCollection.findOne(query);
+        res.send(response);
+      }
+      if (email) {
+        query.email = email;
+        const cursor = testsCollection.find(query);
+        const response = await cursor.toArray();
+        res.send(response);
+      }
+    });
+
+    // update test
+    app.patch("/test/:id", async (req, res) => {
+      const testId = req.params.id;
+      const question = req.query.question;
+      let updatedData = { ...req.body };
+
+      if (question >= 0) {
+        updatedData[`contents.${question}.answered`] = true;
+      }
+
+      const updateOpt = await testsCollection.updateOne(
+        { _id: ObjectID(testId) },
+        {
+          $set: {
+            ...updatedData,
+          },
+        },
+        { upsert: true }
+      );
+
+      res.send(updateOpt);
     });
   } catch (error) {
     console.log(error);
